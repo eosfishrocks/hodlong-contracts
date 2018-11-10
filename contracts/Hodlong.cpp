4b99cb6cf582470b59a4f5fec7d08c684df10901
epicdistraction <daniel@epicdistraction.com>
2018-11-10 14:31:30 -0800

    Updated multiindex table names

--- contracts/Hodlong.cpp
+++ contracts/Hodlong.cpp
@@ -2,10 +2,10 @@
 
 namespace bpfish {
     void hodlong::buy(name buyer, name storage_id) {
-        hodlong::storage storage(_self, _self.value);
+        storage storage_table(_self, _self.value);
 
-        auto iterator = storage.find(storage_id.value);
-        eosio_assert(iterator != storage.end(), "The bid not found");
+        auto iterator = storage_table.find(storage_id.value);
+        eosio_assert(iterator != storage_table.end(), "The bid not found");
         eosio_assert(iterator->accepted_seeders.size() >= iterator->max_seeders,
                      "The storage object has the max amount of seeders");
     }
@@ -13,12 +13,12 @@ namespace bpfish {
     void hodlong::createobj(name account, storage_t newObj) {
         require_auth(account.value);
 
-        storage storage(_self, _self.value);
+        storage storage_table(_self, _self.value);
 
-        auto iterator = storage.find(newObj.storage_id.value);
-        eosio_assert(iterator == storage.end(), "Obj for this ID already exists");
+        auto iterator = storage_table.find(newObj.storage_id.value);
+        eosio_assert(iterator == storage_table.end(), "Obj for this ID already exists");
 
-        storage.emplace(account, [&](auto &storage_obj) {
+        storage_table.emplace(account, [&](auto &storage_obj) {
             storage_obj = newObj;
         });
     }
@@ -26,50 +26,50 @@ namespace bpfish {
     void hodlong::addstats(const name from, const name to, name storage_id, bool seeder, uint64_t amount) {
         require_auth(from);
 
-        pstats pStats(_self, _self.value);
+        pstats pstats_table(_self, _self.value);
         time_t date = now();
         stat
         client_stat = {from, to, amount, seeder};
 
-        auto pstats = pStats.find(storage_id.value);
+        auto pstat_itr = pstats_table.find(storage_id.value);
         bool foundStat = false;
-        if (pstats == pStats.end()) {
-            pStats.emplace(storage_id, [&](auto &tmp_stat) {
+        if (pstat_itr == pstats_table.end()) {
+            pstats_table.emplace(storage_id, [&](auto &tmp_stat) {
                 tmp_stat.storage_id = storage_id;
                 tmp_stat.pending_stats.push_back(client_stat);
             });
             foundStat = true;
         } else {
-            pStats.modify(pstats, from, [&](auto &tmp_stat) {
+            pstats_table.modify(pstat_itr, from, [&](auto &tmp_stat) {
                 tmp_stat.pending_stats.push_back(client_stat);
             });
         }
         //may need to break into deferred actions for delete depending on mainnet processing times
         if (foundStat) {
-            storage storage(_self, storage_id.value);
-            auto storageIterator = storage.find(storage_id.value);
-            eosio_assert(storageIterator == storage.end(), "The storage id is not found");
+            storage storage_table(_self, storage_id.value);
+            auto storageIterator = storage_table.find(storage_id.value);
+            eosio_assert(storageIterator == storage_table.end(), "The storage id is not found");
 
             vector <uint64_t> pendingDeletion;
             // Inefficient loop and due to RAM Cost. Cheaper to offload to cpu
-            for (int v1 = 0; pstats->pending_stats.size(); v1++) {
-                for (int v2 = 1; pstats->pending_stats.size(); v2++) {
+            for (int v1 = 0; pstat_itr->pending_stats.size(); v1++) {
+                for (int v2 = 1; pstat_itr->pending_stats.size(); v2++) {
                     int v3 = v1 + v2;
-                    if ((v1 != v3) && ((pstats->pending_stats[v1].to == pstats->pending_stats[v3].from ||
-                                        pstats->pending_stats[v1].from == pstats->pending_stats[v3].to) &&
-                                       (pstats->pending_stats[v1].to != pstats->pending_stats[v3].to &&
-                                        pstats->pending_stats[v1].from != pstats->pending_stats[v3].to))) {
+                    if ((v1 != v3) && ((pstat_itr->pending_stats[v1].to == pstat_itr->pending_stats[v3].from ||
+                            pstat_itr->pending_stats[v1].from == pstat_itr->pending_stats[v3].to) &&
+                                       (pstat_itr->pending_stats[v1].to != pstat_itr->pending_stats[v3].to &&
+                                               pstat_itr->pending_stats[v1].from != pstat_itr->pending_stats[v3].to))) {
                         // Expire stats in a week.
-                        if (now() - pstats->pending_stats[v1].submitted < 604800) {
+                        if (now() - pstat_itr->pending_stats[v1].submitted < 604800) {
                             pendingDeletion.push_back(v1);
-                        } else if (now() - pstats->pending_stats[v3].submitted < 604800);
+                        } else if (now() - pstat_itr->pending_stats[v3].submitted < 604800);
                         else {
                             uint64_t verifiedAmount;
                             uint64_t verifiedAmountModifier;
-                            if (pstats->pending_stats[v1].amount > pstats->pending_stats[v3].amount) {
-                                verifiedAmount = pstats->pending_stats[v3].amount;
+                            if (pstat_itr->pending_stats[v1].amount > pstat_itr->pending_stats[v3].amount) {
+                                verifiedAmount = pstat_itr->pending_stats[v3].amount;
                                 verifiedAmountModifier =
-                                        pstats->pending_stats[v1].amount - pstats->pending_stats[v3].amount;
+                                        pstat_itr->pending_stats[v1].amount - pstat_itr->pending_stats[v3].amount;
                                 // Search for user account to add
                                 // #TODO Update amount to multiplier times active stats
                                 action(permission_level{from, "active"_n},
@@ -77,11 +77,11 @@ namespace bpfish {
                                        std::make_tuple(_self, to, amount, std::string(""))
                                 ).send();
                             } else {
-                                verifiedAmount = pstats->pending_stats[v1].amount;
+                                verifiedAmount = pstat_itr->pending_stats[v1].amount;
                                 verifiedAmountModifier =
-                                        pstats->pending_stats[v3].amount - pstats->pending_stats[v1].amount;
+                                        pstat_itr->pending_stats[v3].amount - pstat_itr->pending_stats[v1].amount;
                             }
-                            storage.modify(storageIterator, from, [&](auto &storage_stat) {
+                            storage_table.modify(storageIterator, from, [&](auto &storage_stat) {
                                 storage_stat.bandwidth_used += amount;
                             });
                         }
@@ -93,12 +93,12 @@ namespace bpfish {
 
     void hodlong::addaccount(const name account, string &pub_key) {
         require_auth(account);
-        users users(_self, _self.value);
+        users users_table(_self, _self.value);
 
-        auto iterator = users.find(account.value);
-        eosio_assert(iterator == users.end(), "Address for account already exists");
+        auto iterator = users_table.find(account.value);
+        eosio_assert(iterator == users_table.end(), "Address for account already exists");
 
-        users.emplace(account, [&](auto &user) {
+        users_table.emplace(account, [&](auto &user) {
             user.account_name = account;
             user.pub_key = pub_key;
 
@@ -108,24 +108,24 @@ namespace bpfish {
 
     void hodlong::addseed(name account, name storage_id) {
         require_auth(account);
-        users users(_self, _self.value);
+        users users_table(_self, _self.value);
 
-        auto iterator = users.find(account.value);
-        eosio_assert(iterator != users.end(), "Address for account not found");
+        auto iterator = users_table.find(account.value);
+        eosio_assert(iterator != users_table.end(), "Address for account not found");
 
-        users.modify(iterator, account, [&](auto &user) {
+        users_table.modify(iterator, account, [&](auto &user) {
             user.seeded_objects.push_back(storage_id.value);
         });
     }
 
     void hodlong::removeseed(const name account, name storageId) {
         require_auth(account);
-        users users(_self, _self.value);
+        users users_table(_self, _self.value);
 
-        auto iterator = users.begin();
-        eosio_assert(iterator != users.end(), "Address for account not found");
+        auto iterator = users_table.find(account.value);
+        eosio_assert(iterator != users_table.end(), "Address for account not found");
 
-        users.modify(iterator, account, [&](auto &user) {
+        users_table.modify(iterator, account, [&](auto &user) {
 
             auto position = find(user.seeded_objects.begin(), user.seeded_objects.end(), 8);
             if (position != user.seeded_objects.end()) // == myVector.end() means the element was not found
@@ -137,23 +137,23 @@ namespace bpfish {
         if (from == _self)
             return;
 
-        users users(_self, _self.value);
-        auto user = users.find(from.value);
-        eosio_assert(user != users.end(), "User does not exist");
+        users users_table(_self, _self.value);
+        auto user = users_table.find(from.value);
+        eosio_assert(user != users_table.end(), "User does not exist");
 
         // If using a different token, please update name
         if (_code != name("EOS"_n))
             return;
-        users.modify(user, from, [&](auto &newuser) {
+        users_table.modify(user, from, [&](auto &newuser) {
             newuser.balance += quantity;
         });
 
     }
 
     void hodlong::removefunds(name to, asset quantity, string memo) {
-        users users(_self, _self.value);
-        auto user = users.find(to.value);
-        eosio_assert(user != users.end(), "User does not exist");
+        users users_table(_self, _self.value);
+        auto user = users_table.find(to.value);
+        eosio_assert(user != users_table.end(), "User does not exist");
         eosio_assert(user->balance <= quantity, "You do not have the required balance to remove the funds");
         action(permission_level{_self, "active"_n},
                "eosio.token"_n, "transfer"_n,
